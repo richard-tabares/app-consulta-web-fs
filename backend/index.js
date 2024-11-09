69a4203526ad4bfdb0f75a48e863daa61dfbc323
RichardTB <richardtabaresb@gmail.com>
2024-11-09 01:13:28 -0500

    servidor en node para hacer la peticion al endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+// import fetch from 'node-fetch'
 
 const app = express()
 
@@ -8,12 +9,54 @@ app.use(cors())
 
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/data/:query', async (req, res) => {
+    const query = req.params.query
+    console.log(query)
 
-    const data = {
-        message: 'We are Under Construction We are improving our website We will be back as soon as possible with new cool features!'
+    const url = 'http://10.1.6.16:9001/SpoaWebServices/ConsultaConveniosWS'
+    const param = '?invoke=consultaConvenio&param0='
+    const xml = `<ConsultaConveniosWS>
+    <EncabezadoConsulta>
+    <Ip>192.168.45.199</Ip>
+    <Mac>2c:41:39:a1:f8:e9</Mac>
+    <Host>pc47</Host>
+    <NombreConvenio>ConsultaWeb_WS</NombreConvenio>
+    <Usuario>CONSULTAWEB_WS</Usuario>
+    <IdAuditoria>123456</IdAuditoria>
+    </EncabezadoConsulta>
+    <ParametrosConsulta>
+    <Parametro>
+    <Llave>caso_noticia</Llave>
+    <Valor>11001609925219990004157</Valor>
+    </Parametro>
+    </ParametrosConsulta>
+    </ConsultaConveniosWS>`
+
+    const urlComplete = url + param + xml
+
+    console.log(url)
+    const user = 'administrador'
+    const pass = '123'
+    const headers = {
+        'Authorization': `Basic ${Buffer.from(`${user}:${pass}`).toString('base64')}`,
+        'Content-Type': 'text/xml'
     }
-    res.json(data)
+
+    try {   
+        const response = await fetch(urlComplete, {
+            method: 'GET',
+            headers: headers,
+        });
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+
+        const data = await response.text();
+        res.send(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al consultar la API', message: error.message });
+    }
+
 })
 
 app.listen(port, () => {
